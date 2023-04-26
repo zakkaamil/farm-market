ef6bc0e246f9b7a4db47005ad561d5f0f96f7b5b
Kamil Żak <zak.kaamil@gmail.com>
2023-04-26 18:40:25 +0200

    Update fruits view

--- src/app/fruits/fruits.component.ts
+++ src/app/fruits/fruits.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Item } from '../models/item.interface';
 
 @Component({
   selector: 'app-fruits',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./fruits.component.scss']
 })
 export class FruitsComponent {
-
+  items: Item[] = [
+    {
+      name: 'Apple',
+      description: `An apple is an edible fruit produced by an apple tree (Malus domestica). 
+        Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus.`,
+      isActive: true
+    },
+    {
+      name: 'Orange',
+      description: 'An orange is a fruit of various citrus species in the family Rutaceae.',
+      isActive: true
+    },
+    {
+      name: 'Banana',
+      description: `A banana is an elongated, edible fruit botanically a berry – 
+        produced by several kinds of large herbaceous flowering plants in the genus Musa. 
+        In some countries, bananas used for cooking may be called "plantains", distinguishing them from dessert bananas.`,
+      isActive: false
+    }
+  ]
 }
